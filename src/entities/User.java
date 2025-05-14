5e6e45730252ee605950ba6c8ddd1f0141ac5fb1
Osezele Oghedegbe <oghedegbeosezele@gmail.com>
2025-05-14 16:06:38 +0100

    feat: add send, react and reply to message

--- src/entities/User.java
+++ src/entities/User.java
@@ -17,6 +17,22 @@ public class User {
         this.chats = new ArrayList<>();
     }
 
+    public ChatMessage sendTextMessage(Chat chat, String content) {
+        Text text = new Text(content);
+        return new ChatMessage(chat, text, this);
+    }
+
+    public ChatMessage reactToChatMessage(Reaction reaction, ChatMessage message ) {
+        message.getReactions().add(reaction);
+        return message;
+    }
+
+    public ChatMessage replyToChatMessage(ChatMessage message, String content) {
+        ChatMessage reply = new ChatMessage(message.getChat(), new Text(content), this);
+        message.getReplies().add(reply);
+        return reply;
+    }
+
     // Getter And Setter
     public long getId() {
         return id;
